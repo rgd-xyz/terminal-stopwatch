3fe68392a735492ae01ce796bbbab5a58e902df9
rgd <tc546649@gmail.com>
2025-10-02 00:12:13 +0530

    initial commit: simple stopwatch

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# installer for stopwatch
+
+chmod +x stopwatch
+sudo cp stopwatch /usr/local/bin/
+
+echo "stopwatch installed! run it anywhere with:"
+echo "stopwatch"
+
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# uninstaller for stopwatch
+
+sudo rm -f /usr/local/bin/stopwatch
+echo " stopwatch removed!"
+
